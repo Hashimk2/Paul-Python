4239e6c34fa7619fcc7a24dce28404ed8549aa84
Hashim Kidwai <hashimkidwai@gmail.com>
2024-07-02 17:56:05 -0500

    First commit

---
+++ ADAhomework-_-.py
@@ -0,0 +1,239 @@
+# # # # grades = ['34','43','9','67','100']
+# # # # print(grades)
+# # # # print(grades[2])
+# # # # grades[2]=['78']  question 1
+
+# # # # print(grades)
+# # # # print(grades[2])
+
+
+
+# # # # grades = ['34','43','9','67','100']
+# # # # print(grades[0]+ ' ' +grades[4])
+# # # # print(grades[-2])
+
+
+
+# # # # fruits = ['apple','banana','orange','grape']
+# # # # print(fruits[1])
+# # # # print(type(fruits[0][2]))
+# # # # string1 = 'Artemis'
+# # # # print([0])
+
+
+
+# # # # picture=[[1,2,3],
+# # # #          [4,5,6],
+# # # #          [7,8,9]]
+# # # # print(picture)
+# # # # print(picture[1][0])
+# # # # (picture[0][0])=2
+# # # # print(picture)
+
+
+
+# # # numbers = []
+# # # numbers.append(99)
+# # # numbers.append(98)
+# # # numbers.append(99)
+# # # numbers.append(100)
+# # # numbers.append(100)
+
+# # # print (numbers)
+
+
+
+
+# # # # Strings '123' "123" """123"""
+# # # # Floating-point values (decimal values) <-- these are called either doubles or float
+# # # # ints (integers) wholenumbers postivie or negative
+# # # # chars 'a' '@' '1'
+# # lists ['a', 'b']
+# # # # 
+
+
+
+
+
+
+# # user_input = input('Input your first number(0 - 9): ')
+# # # print(user_input)
+# # a = 0
+# # b = 1
+# # c = 2
+# # d = 3
+# # e = 4
+# # f = 5
+# # g = 6
+# # h = 7
+# # i = 8
+# # j = 9
+
+
+
+# # if (user_input == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # if (user_input == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # if (user_input == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # if (user_input == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # if (user_input == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # if (user_input == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # if (user_input == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # if (user_input == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # if (user_input == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # if (user_input == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # else:
+# #   print('YOU ARE NOT FOLLOWING THE INSTRUCTIONS!')
+
+# # user_input_2 = input('Input your second number(0 - 9): ')
+# # print(user_input)
+
+# # if (user_input_2 == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # if (user_input_2 == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # if (user_input_2 == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # if (user_input_2 == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # if (user_input_2 == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # if (user_input_2 == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # if (user_input_2 == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # if (user_input_2 == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # if (user_input_2 == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # if (user_input_2 == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print('')
+# # else:
+# #   print('YOU ARE NOT FOLLOWING THE INSTRUCTIONS!')
+
+# # if (user_input + user_input_2 == 'a','b','c','d','e','f','g','h','i','j'):
+# #   print(user_input, '+' ,user_input_2)
+
+
+
+# # first_number = input('Please input your first number: ')
+# # print('Your number is' ,first_number )
+# # second_number = input('Please input your second number: ')
+# # print('Your number is' ,second_number )
+
+
+
+# # sum = int(first_number) + int(second_number)
+# # # sum2 = 4 + 9
+
+# # # print('4 + 9 =', sum2)
+# # print(first_number , '+'  ,second_number, '=' ,sum )
+
+
+
+# # first_number = input('Please input your first number: ')
+# # print('Your number is' ,first_number )
+# # second_number = input('Please input your second number: ')
+# # print('Your number is' ,second_number )
+# # try:
+# #   sum = int(first_number) + int(second_number)
+# #   print(first_number , '+'  ,second_number, '=' ,sum )
+# # except Exception as e:
+# #   print('YOU ARE NOT FOLLOWING THE INSTRUCTIONS! PLEASE RETRY WITH AN INTEGER!')
+
+
+# # try:
+# #   first_number = input('Please input your first number: ')
+
+# #   second_number = input('Please input your second number: ')
+
+# #   sum = int(first_number) + int(second_number)
+  
+# #   print('Your number is' ,first_number )
+# #   print('Your number is' ,second_number )
+# #   print(first_number , '+'  ,second_number, '=' ,sum )
+# # except:
+# #   print('YOU ARE NOT FOLLOWING THE INSTRUCTIONS! PLEASE RETRY WITH AN INTEGER!')
+
+
+
+
+# # if (condition):
+# #   # code that executes iff condition is true
+# # else:
+# #   # code that executes iff condition is false
+
+# # and or not
+# # operand1 or operand2
+
+  
+# user_input = int(input('Please enter a number: '))
+# remainder = user_input % 2
+# if(remainder == 0):
+#   print('Your number is even')
+# if(remainder == 1):
+#   print('Your number is odd')
+
+
+  
+
+# # while
+# # for
+
+# # while (condition):
+# #   # code that executes WHILE the condition is true
+
+# # number = 2
+# # while (number >= 0):
+# #   print(number, end='')
+# #   number -= 1
+
+# # user_input = int(input('Enter the number 7: '))
+# # while (user_input != 7):
+# #   print('Bad Boy')
+# #   user_input = int(input('Your number was not 7, please enter the number 7: '))
+
+# # for i in range(1,10):
+# #   print(i)
+
+# string1 = 'hashim'
+# for letter in string1:
+#   print(letter)
+  
+
+
+# myColor = input('Please input your color: ')
+# if(myColor == 'Red' or myColor == 'RED' or myColor == 'red'):
+#   print('Your color is red!')
+# if(myColor != 'Red' and  myColor != 'RED' and  myColor != 'red'):
+#   print('Your color is NOT red!')
+
+# user_input = int(input('Please input your number: '))
+# if (user_input >4 and user_input <10):
+#   print('Your number is between 5-10')
+# if (user_input <4 or user_input >10): # really good check, althuogh if the user entered a letter(s), what's the problem?
+#   print('Your number is NOT between 5-10')
+
+my_number = int(input('Please input your number: '))
+remainder = my_number % 2
+
+if (my_number > 0 and remainder == 0):
+  print('You have an even positive number')
+if (my_number > 0 and remainder == 1):
+  print('You have an odd positive number')
+if (my_number < 0 and remainder == 0):
+  print('You have an even negative number')
+if (my_number < 0 and remainder == 1):
+  print('You have an odd negative number')
+if (my_number == 0):
+  print('Your number is zero')
+
